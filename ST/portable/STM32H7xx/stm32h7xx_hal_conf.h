de708f526e2d11aee8442daad873c4a661c19b17
GeneKong <gyx_edu@qq.com>
2018-08-14 17:56:49 +0800

    update for template file...

--- ST/portable/STM32H7xx/stm32h7xx_hal_conf.h
+++ ST/portable/STM32H7xx/stm32h7xx_hal_conf.h
@@ -415,9 +415,9 @@
   *         If expr is true, it returns no value.
   * @retval None
   */
-  #define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
+  #define assert_param(expr) ((expr) ? (void)0 : assert_failed((const char *)__FILE__, __LINE__))
 /* Exported functions ------------------------------------------------------- */
-  void assert_failed(uint8_t* file, uint32_t line);
+  void assert_failed(const char* file, uint32_t line);
 #else
   #define assert_param(expr) ((void)0)
 #endif /* USE_FULL_ASSERT */
--- ST/portable/STM32L4xx/stm32l4xx_hal_conf.h
+++ ST/portable/STM32L4xx/stm32l4xx_hal_conf.h
@@ -458,9 +458,9 @@
   *         If expr is true, it returns no value.
   * @retval None
   */
-  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((char *)__FILE__, __LINE__))
+  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((const char *)__FILE__, __LINE__))
 /* Exported functions ------------------------------------------------------- */
-  void assert_failed(char *file, uint32_t line);
+  void assert_failed(const char *file, uint32_t line);
 #else
   #define assert_param(expr) ((void)0U)
 #endif /* USE_FULL_ASSERT */
